bcf882a8249aa117efb932c6ff8a72ee31374412
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2022-02-17 10:56:28 -0600

    control statements & loops

---
+++ src/ControlStatementsLoops.java
@@ -0,0 +1,70 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ControlStatementsLoops {
+    public static void main(String[] args) {
+        
+        // CONTROL STATEMENTS
+        // allows us to control the flow of the code
+        // operators - tools needed to control the flow
+        // ==, !=, > . < <= , >= // logical operators
+        
+        // boolean expression
+        // java statement that results in either true or false
+//        int myFavNum = 18; // statement
+//        boolean yorn = myFavNum > 50; //  result in a boolean
+//        System.out.println(yorn);
+        
+        // logical operators
+        // &&, ||,  &, |,  !
+        
+//        // if statements
+//        int age = 11;
+//        int bmi = 20;
+//        String myHair = "curly";
+//        String yourHair = "curly";
+//        if(myHair.equals(yourHair)){
+//            System.out.println("ahh fun times!");
+//        } else {
+//            System.out.println("other");
+//        }
+        // COME BACK TO THIS - WHY DOES IT WORK
+    
+        
+        // STRING COMPARISONS
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Continue? [y/N] ");
+//        String userInput = sc.next();
+//
+//// Don't do this!
+//        boolean confirmation = userInput == "y";
+//
+//        if(userInput.equals("y")){
+//            System.out.println("yes continue");
+//        } else {
+//            System.out.println("don't continue");
+//        }
+
+//        String myName = "Laura";
+//        String yourName = "laura";
+//
+//        boolean nameMatching  = myName.equals(yourName);
+////        System.out.println(nameMatching); // false
+//        boolean nameNoCase = myName.equalsIgnoreCase(yourName); //
+////        System.out.println(nameNoCase); //true
+//
+//        boolean bothNames = myName.equals(yourName);
+//        System.out.println(bothNames);
+//
+//        boolean moreNames = myName.equals(yourName.toUpperCase(Locale.ROOT));
+//        System.out.println(moreNames.toUpperCase()); // not going to work
+//
+//        yourName.toUpperCase(Locale.ROOT);
+//        myName.toUpperCase(Locale.ROOT);
+//        if(myName.equals(yourName)){
+//            System.out.println("same name");
+//        }
+        
+        
+    }
+}
